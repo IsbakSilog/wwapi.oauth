eae7052655f337593fe0d537813fe11111d190f9
Carl Angelo Cabahug <ca.cabahug@gmail.com>
2024-12-09 14:33:34 +0800

    oauth

---
+++ src/test/java/com/mynt/isbak/Oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynt.isbak.Oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
